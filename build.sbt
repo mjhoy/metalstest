05ce58a78132bc913e34b0e645b287580f206917
Mikey Hoy <mjh@mjhoy.com>
2024-09-11 08:23:48 -0400

    attempt 1

--- build.sbt
+++ build.sbt
@@ -1,7 +1,9 @@
-
 // The simplest possible sbt build file is just one line:
 
-scalaVersion := "2.13.12"
+ThisBuild / scalaVersion := "2.13.14"
+ThisBuild / semanticdbEnabled := true
+ThisBuild / semanticdbVersion := "4.9.3"
+
 // That is, to create a valid sbt build, all you've got to do is define the
 // version of Scala you'd like your project to use.
 
@@ -21,11 +23,12 @@ version := "1.0"
 // mostly only necessary if you intend to publish your library's binaries on a
 // place like Sonatype.
 
-
 // Want to use a published library in your project?
 // You can define other libraries as dependencies in your build like this:
 
-libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "2.3.0"
+libraryDependencies ++= Seq(
+  "org.typelevel" %% "cats-core" % "2.10.0"
+)
 
 // Here, `libraryDependencies` is a set of dependencies, and by using `+=`,
 // we're adding the scala-parser-combinators dependency to the set of dependencies
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,51 @@
+import cats.Monad
+import cats.MonadError
+import cats.implicits._
+
+sealed trait MyAppM[+a] {}
+
+object MyAppM {
+  implicit def myAppMMonad: Monad[MyAppM] = new Monad[MyAppM] {
+    override def flatMap[A, B](fa: MyAppM[A])(f: A => MyAppM[B]): MyAppM[B] =
+      ???
+
+    override def tailRecM[A, B](a: A)(f: A => MyAppM[Either[A, B]]): MyAppM[B] =
+      ???
+
+    override def pure[A](x: A): MyAppM[A] = ???
+  }
+
+  implicit def myAppMMonadError: MonadError[MyAppM, String] =
+    new MonadError[MyAppM, String] {
+
+      override def pure[A](x: A): MyAppM[A] = ???
+
+      override def raiseError[A](e: String): MyAppM[A] = ???
+
+      override def handleErrorWith[A](fa: MyAppM[A])(
+          f: String => MyAppM[A]
+      ): MyAppM[A] = ???
+
+      override def flatMap[A, B](fa: MyAppM[A])(f: A => MyAppM[B]): MyAppM[B] =
+        ???
+
+      override def tailRecM[A, B](a: A)(
+          f: A => MyAppM[Either[A, B]]
+      ): MyAppM[B] = ???
+
+    }
+}
+
 object Main extends App {
   println("Hello, World!")
+}
+
+object Test {
+  def action(a: String): MyAppM[Int] = ???
+
+  def runTraverse(strs: Seq[String]): MyAppM[Seq[Int]] = {
+    strs.traverse { str =>
+      action(str)
+    }
+  }
 }
